9c52b7bd5d900bacc42256466896564615dd35a2
glnKresna <prabandikakresna@gmail.com>
2026-05-08 21:58:15 +0700

    chore: add id, label, icon to projects array

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,27 +1,14 @@
 import { create } from "zustand";
-
-export interface Project {
-    id: string;
-    name: string;
-    icon: string;
-}
-
-interface SuperchargedStore {
-    projects: Project[];
-    activeProject: string | null;
-    chatMapping: Record<string, string>;
-    addProject: (name: string, icon: string) => void;
-    setActiveProject: (id: string | null) => void;
-}
+import type { SuperchargedStore } from "../types/types";
 
 export const useStore = create <SuperchargedStore>((set) => ({
     projects: [
-        { id: "proj_1", name: "Coding", icon: ""},
-        { id: "proj_2", name: "Research", icon: ""},
-        { id: "proj_3", name: "Writing", icon: ""},
-        { id: "proj_4", name: "Health", icon: ""},
-        { id: "proj_5", name: "Design", icon: ""},
-        { id: "proj_6", name: "Entertainment", icon: ""},
+        { id: "proj_1", name: "Coding", icon: "👩‍💻"},
+        { id: "proj_2", name: "Study", icon: "🔬"},
+        { id: "proj_3", name: "Writing", icon: "📑"},
+        { id: "proj_4", name: "Health", icon: "💪"},
+        { id: "proj_5", name: "Business", icon: "💲"},
+        { id: "proj_6", name: "Entertainment", icon: "🎮"},
     ],
 
     activeProject: null,
